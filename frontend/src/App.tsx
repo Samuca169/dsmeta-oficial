33e92f8e58aa0856aee02c8718e4b5adbf81687f
Samuca169 <samucabarr169@gmail.com>
2023-09-05 12:24:31 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
     return(
-      <h1>Ola Mundo!</h1>
+      <>
+        <h1>Ola Mundo!</h1>
+        <NotificationButton />
+        <NotificationButton />
+        <NotificationButton />
+        <NotificationButton />
+        <NotificationButton />
+      </>
     )
 }
 
